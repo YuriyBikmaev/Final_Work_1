af0f4ce9bb8913dc3a42123f69d5e6f3d51b6c68
y.bikmaev@yandex.ru <y.bikmaev@yandex.ru>
2022-09-29 11:18:35 +0400

    Написана программа реализации поставленной задачи

---
+++ FinalProject/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. 
+
+string[] SelectionElementToNewArray(string[] array, int size)
+{
+    string[] resultArray = new string[0];
+
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= size)
+        {
+            Array.Resize(ref resultArray, resultArray.Length + 1);
+            resultArray[j] = array[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+
+string[] sourceArray = { "Hello", "2", "world", ":-)" };
+Console.WriteLine(String.Join(", ", SelectionElementToNewArray(sourceArray, 3)));
